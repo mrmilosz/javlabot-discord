c52115208856c6c89ff02900d3330e9a677e2362
mrmilosz <milosz@milosz.ca>
2018-06-09 17:30:13 -0400

    Avoid error when echoed message is empty/null
    Shouldn't try to send it in that situation.

--- commands/echo.js
+++ commands/echo.js
@@ -1,5 +1,7 @@
 module.exports = {
     run: (message, argument) => {
-        message.channel.send(argument);
+        if (argument) {
+            message.channel.send(argument);
+        }
     }
 };
