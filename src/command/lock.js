2e9c9621bb1878ca715dac2cf86a9638c2ff34d5
mrmilosz <milosz@milosz.ca>
2018-06-10 16:13:50 -0400

    Implement single-instance mode with no locks
    This is probably better for first-time users of the bot.

--- src/command/lock.js
+++ src/command/lock.js
@@ -8,7 +8,10 @@ module.exports = {
     @notbot
     @authenticated(authorId => authorId === config.authorDiscordId)
     run(message, argument) {
-        if (argument === config.lockId) {
+        if (config.lockId === undefined) {
+            message.channel.send('No lock; running in single-instance mode');
+            state.haveLock = true;
+        } else if (argument === config.lockId) {
             message.channel.send(`Lock acquired: ${config.lockId}`);
             state.haveLock = true;
         } else {
