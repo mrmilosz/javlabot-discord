79f8e53cf875dfd3dd4588a876793fa1506eaea0
mrmilosz <milosz@milosz.ca>
2018-06-10 15:43:07 -0400

    Annotate lock method with @notbot
    Prettier when there is no exception in this situation (e.g. when someone
    writes !!echo !!lock).

--- src/command/lock.js
+++ src/command/lock.js
@@ -1,9 +1,11 @@
 const authenticated = require('./decorator/authenticated');
 const config = require('../../config');
 const logger = require('../logger').get(module);
+const notbot = require('./decorator/notbot');
 const state = require('../state');
 
 module.exports = {
+    @notbot
     @authenticated(authorId => authorId === config.authorDiscordId)
     run(message, argument) {
         if (argument === config.lockId) {
