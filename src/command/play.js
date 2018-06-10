1e8bce9cfa085d5c3ab4d237e701ff4ff9bafc4d
mrmilosz <milosz@milosz.ca>
2018-06-10 15:19:46 -0400

    Implement sound pipeline
    This is so sounds don't play simultaneously. Moreover, procedures have
    been put in place to prevent infinite sound pipelining.

--- src/command/play.js
+++ src/command/play.js
@@ -5,37 +5,60 @@ const logger = require('../logger').get(module);
 const notbot = require('./decorator/notbot');
 const path = require('path');
 
+const MAX_SOUND_COUNT = 3;
+
+let currentSoundCount = 0;
+let readyToPlay = Promise.resolve();
+
 module.exports = {
     @locked
     @notbot
     run(message, argument) {
-        if (!argument.includes('/')) {
-            const soundPath = path.join('sound', argument);
-            fs.exists(soundPath, exists => {
-                if (exists) {
-                    if (message.member.voiceChannel !== undefined) {
-                        message.member.voiceChannel.join().then(connection => {
-                            const dispatcher = connection.playFile(soundPath);
-                            dispatcher.on('end', reason => {
-                                message.member.voiceChannel.leave();
-                            });
-                        }).catch(caught => {
-                            logger.warn(`Could not play sound: ${caught}`);
-                            message.member.voiceChannel.leave();
-                            handleInvalidFile(message.channel, argument);
-                        });
-                    } else {
-                        message.reply('you must be in a voice channel to use this command!');
-                    }
-                } else {
-                    logger.info(`No such file: ${soundPath}`);
-                    handleInvalidFile(message.channel, argument);
-                }
-            });
-        } else {
+        if (message.member.voiceChannel === undefined) {
+            message.reply('you must be in a voice channel to use this command!');
+            return;
+        }
+
+        if (argument.includes('/')) {
             logger.debug(`Invalid filename: ${argument}`);
             handleInvalidFile(message.channel, argument);
+            return;
         }
+
+        const soundPath = path.join('sound', argument);
+
+        if (currentSoundCount >= MAX_SOUND_COUNT) {
+            message.channel.send('Too many sounds! Wait until there are fewer in the pipeline.');
+            return;
+        }
+
+        ++currentSoundCount;
+
+        readyToPlay = readyToPlay.then(() => new Promise(resolve => {
+            fs.exists(soundPath, fileExists => {
+                if (!fileExists) {
+                    logger.info(`No such file: ${soundPath}`);
+                    handleInvalidFile(message.channel, argument);
+                    resolve();
+                    return;
+                }
+
+                message.member.voiceChannel.join().then(connection => {
+                    const dispatcher = connection.playFile(soundPath);
+                    dispatcher.on('end', reason => {
+                        message.member.voiceChannel.leave();
+                        --currentSoundCount;
+                        resolve();
+                    });
+                }).catch(caught => {
+                    logger.warn(`Could not play sound: ${caught}`);
+                    message.member.voiceChannel.leave();
+                    handleInvalidFile(message.channel, argument);
+                    --currentSoundCount;
+                    resolve();
+                });
+            });
+        }));
     }
 };
 
